42960b1e267357391b9241d5237baa70a3bfb4ba
filipedonde <filipedonde@hotmail.com>
2017-12-16 11:19:54 -0200

    Update pronto

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
     def new
+        @article = Article.new
     end
 
     def index
@@ -11,12 +12,28 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article=Article.new(article_params)    
-
-        @article.save
-
-        redirect_to @article        
+        @article = Article.new(article_params)
+       
+        if @article.save
+          redirect_to @article
+        else
+          render 'new'
+        end
     end
+
+    def edit
+        @article=Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
+      end
 
     private
         def article_params
